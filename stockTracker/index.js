cc882aee6af091602518312da898b4a12a5698ad
maksthorn <maksdrzezdzon@gmail.com>
2019-12-09 12:07:37 +0000

    getting started with handlebars

--- stockTracker/index.js
+++ stockTracker/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 5000;
 
 // point to folder name
 app.use(express.static(path.join(__dirname, 'site')));
+
+// handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+// mapping
+app.get('/', function(req, res){
+	res.render('home');
+});
 
 app.listen(PORT, () => console.log('Server listening on port ' + PORT))
